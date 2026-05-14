987e04546214b43e7b744791056fe6919f864c9d
azroukarim <azroukarim@example.com>
2026-05-14 18:02:15 +0100

    Change splash ad duration to 3s

--- components/home/SplashAd.tsx
+++ components/home/SplashAd.tsx
@@ -35,10 +35,10 @@ export default function SplashAd() {
             setIsClosing(false);
             setTimeout(() => setIsVisible(true), 800);
             
-            // Auto-close after 2 seconds
+            // Auto-close after 3 seconds
             setTimeout(() => {
               handleClose();
-            }, 2000);
+            }, 3000);
           }
       })
       .catch(err => console.error("Splash fetch error:", err));
